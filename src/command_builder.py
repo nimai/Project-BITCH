c4220f29ac79fbe56f091ba352202eccd2fee437
Arnaud Theismann <arnaud.theismann@gmail.com>
2011-11-28 15:09:11 +0100

    write data ok

--- src/command_builder.py
+++ src/command_builder.py
@@ -33,6 +33,11 @@ def authentication_2nd_step_apdu(deciphered_data):
     res.append(0x00)
     return res 
 
+def format_PICC_apdu():
+    nbytes = 8
+    return [0xFF, 0x00, 0x00, 0x00, nbytes, 0xD4, 0x40, 0x01, 0x90, 0xFC, 0x00, 0x00, 0x00]               
+
+
 def create_file_apdu(file_no, com_set, acc_rights, file_size):
     res = [0xFF, 0x00, 0x00, 0x00, 0x10, 0xD4, 0x40, 0x01, 0x90, 0xCD, 0x00, 0x00, 0x07]
     res.append(file_no)
@@ -40,7 +45,41 @@ def create_file_apdu(file_no, com_set, acc_rights, file_size):
     res.extend(acc_rights)
     res.extend(file_size)
     res.append(0x00)
-    return res;
-   
-            
+    return res
+
+def write_data_1st_step_apdu(file_no, offset, length, deciphered_data):
+    data_len = 7 + len(deciphered_data)
+    nbytes = 9 + data_len        
+    res = [0xFF, 0x00, 0x00, 0x00, nbytes, 0xD4, 0x40, 0x01, 0x90, 0x3D, 0x00, 0x00, data_len]
+    res.append(file_no)
+    res.extend(offset)
+    res.extend(length)
+    res.extend(deciphered_data)
+    res.append(0x00)
+    return res
+  
+         
+def write_data_2nd_step_adu(deciphered_data):
+    data_len = len(deciphered_data)
+    nbytes = 8 + data_len    
+    res = [0xFF, 0x00, 0x00, 0x00, nbytes, 0xD4, 0x40, 0x01, 0x90, 0xAF, 0x00, 0x00, data_len]
+    res.extend(deciphered_data)
+    res.append(0x00)
+    return res
+
+def read_data_1st_step_apdu(file_no, offset, length):
+    data_len = 7
+    nbytes = 8 + data_len
+    res = [0xFF, 0x00, 0x00, 0x00, nbytes, 0xD4, 0x40, 0x01, 0x90, 0xBD, 0x00, 0x00, data_len]
+    res.append(file_no)
+    res.extend(offset)
+    res.extend(length)
+    res.append(0x00)
+    return res
+
+def read_data_2nd_step_apdu():
+    data_len = 0
+    nbytes = 9
+    return [0xFF, 0x00, 0x00, 0x00, nbytes, 0xD4, 0x40, 0x01, 0x90, 0xAF, 0x00, 0x00, data_len, 0x00]    
+  
 
--- src/loyalty_card.py
+++ src/loyalty_card.py
@@ -7,10 +7,12 @@ from smartcard.CardRequest import CardRequest
 from smartcard.Exceptions import *
 from smartcard.util import toHexString, toBytes
 from binascii import hexlify, unhexlify 
-from command_builder import * 
 from Crypto.Cipher import DES
 import Crypto.Random.random
 from Crypto.Random.random import *
+
+from command_builder import * 
+from crypto import * 
 import struct
 
 
@@ -21,11 +23,21 @@ def perform_command(conn, apdu):
     print 'response: ', toHexString(response), ' status words: ', "%x %x" % (sw1, sw2)
     return response, sw1, sw2
 
-def bytes_to_str(array):
+def bytes_to_hexstr(array):
     return toHexString(array).replace(" ","").lower()  
 
-def str_to_bytes(string):
+def hexstr_to_bytes(string):
     return map(ord, string.decode("hex"))
+
+def str_to_bytes(string):
+    return map(ord, string.decode("ascii"))
+
+def bytes_to_str(array):
+    res = ""
+    for x in array:
+        res += chr(x)
+    return res
+    
 
 def int_to_bytes(integer):
     res = []
@@ -34,24 +46,28 @@ def int_to_bytes(integer):
     padding = 0
     if not len(binary_string) % 8 == 0:
         padding =  8 - len(binary_string) % 8
-    binary_string = ("0"*padding)+binary_string
-    print binary_string
-    for i in range(0,(len(binary_string)/8)):  
-        tmp = binary_string[i:i+8]       
+    binary_string = ("0"*padding)+binary_string    
+    i = 0
+    while not i == (len(binary_string)):    
+        tmp = binary_string[i:i+8]            
         res.append(int(tmp, 2))
-        i+=8
-	print tmp	
+        i+=8		
     return res
     
+def int_to_bytes_with_padding(integer, n):
+    res = int_to_bytes(integer)
+    for i in range(0, n-len(res)):
+        res.append(0)
+    return res   
 
-def perform_DES_CBC_send_mode(key, cipher_text):
-    padding = unhexlify("00"*8)
+def perform_authentication(key, cipher_text):
+    iv = unhexlify("00"*8)
     #a
-    des = DES.new(key, DES.MODE_CBC, padding) 
+    des = DES.new(key, DES.MODE_CBC, iv) 
     nt = des.decrypt(cipher_text)       
     nt2 = nt[1:]+nt[:1]    
     # b        
-    des = DES.new(key, DES.MODE_CBC, padding)
+    des = DES.new(key, DES.MODE_CBC, iv)
     nr=unhexlify(str(StrongRandom().randint(1000000000000000,9999999999999999)))        
     D1=des.decrypt(nr)      
     #c
@@ -59,11 +75,30 @@ def perform_DES_CBC_send_mode(key, cipher_text):
     longlongint2=struct.unpack('>Q',struct.pack('8s', nt2))[0]
     buff=struct.unpack('8s',struct.pack('>Q', longlongint1 ^ longlongint2))[0]     
     # d
-    des = DES.new(key, DES.MODE_CBC, padding)
+    des = DES.new(key, DES.MODE_CBC, iv)
     D2=des.decrypt(buff)    
     #e		
     return nt, nt2, nr, D1, D2  
-    
+
+def xor(a,b):
+    longlongint1=struct.unpack('>Q',struct.pack('8s', a))[0]
+    longlongint2=struct.unpack('>Q',struct.pack('8s', b))[0]
+    buff=struct.unpack('8s',struct.pack('>Q', longlongint1 ^ longlongint2))[0]  
+    return buff
+
+def decipher_CBC_send_mode(session_key, data):
+    res = ""
+    iv = unhexlify("00"*8)
+    des = DES.new(session_key, DES.MODE_CBC, iv)
+    d = des.decrypt(data[0:8])
+    res+=d
+    i=8
+    while not i == len(data):
+        des = DES.new(session_key, DES.MODE_CBC, iv)  
+        d = des.decrypt(xor(d, data[i:i+8])) 
+        res+=d
+        i+=8    
+    return res
 
 class TagException(Exception):    
     def __init__(self, msg):        
@@ -102,14 +137,19 @@ class LoyaltyCard:
             raise TagException('Application deletion has failed!')        
 
     def __create_file(self, file_no, com_set, acc_rights, file_size):
-        fs = int_to_bytes(file_size)
-        for i in range(0, 3-len(fs)):
-            fs.append(0)        
+        fs = int_to_bytes_with_padding(file_size, 3)                     
         apdu = create_file_apdu(file_no, com_set, acc_rights, fs)
         print "create file"
         response, sw1, sw2 = perform_command(self.__connection, apdu)
         if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
             raise TagException('File creation has failed!')
+
+    def __erase_memory(self):
+        apdu = format_PICC_apdu()
+        print "erase memory"
+        response, sw1, sw2 = perform_command(self.__connection, apdu)
+        if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
+            raise TagException('PICC formating has failed')
 
     def __change_key(self, aid, key_no, new_key):
         pass
@@ -121,17 +161,17 @@ class LoyaltyCard:
         if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0xAF and sw1 == 0x90 and sw2 == 0x00):            
             raise TagException('Authentication has failed (1st step)!')
         
-        cyper_text = unhexlify(bytes_to_str(response[3:11]))        
-        nt, nt2, nr, D1, D2 = perform_DES_CBC_send_mode(key, cyper_text) 
+        cyper_text = unhexlify(bytes_to_hexstr(response[3:11]))        
+        nt, nt2, nr, D1, D2 = perform_authentication(key, cyper_text) 
         deciphered_data = hexlify(D1)+hexlify(D2) 	
-        apdu = authentication_2nd_step_apdu(str_to_bytes(deciphered_data))
+        apdu = authentication_2nd_step_apdu(hexstr_to_bytes(deciphered_data))
         print "authentication 2nd step"
         response, sw1, sw2 = perform_command(self.__connection, apdu)
         if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
             raise TagException('Authentication has failed (2nd step)!')
 
         des = DES.new(key, DES.MODE_CBC, unhexlify("00"*8)) 
-        nr2 = des.decrypt(unhexlify(bytes_to_str(response[3:11])))
+        nr2 = des.decrypt(unhexlify(bytes_to_hexstr(response[3:11])))
         if not nr2 == nr[1:]+nr[:1]:
             raise TagException('Authentication has failed (2nd step)!')
         
@@ -141,10 +181,48 @@ class LoyaltyCard:
         return hexlify(nr[0:4]) + hexlify(nt[0:4]) + hexlify(nr[4:8]) + hexlify(nt[4:8])
         
 
-    def __write_data(self, aid, file_no, data, key):
-        pass
+    def __write_data(self, file_no, offset, data, key):
+        padding = 0
+        bdata = str_to_bytes(data)     
+        data_len = len(bdata)
+        crc = crc16_iso14443a(data)#[::-1]   
+        bdata.extend(crc)
+        if not len(bdata) % 8 == 0:
+            padding = 8 - (len(bdata) % 8)
+        for i in range(0, padding):
+            bdata.append(0x00)
+        print "p: ", bytes_to_hexstr(bdata)
+        print "c: ", hexlify(decipher_CBC_send_mode(key, bytes_to_str(bdata)))
+        deciphered_data = hexstr_to_bytes(hexlify(decipher_CBC_send_mode(key, bytes_to_str(bdata)))) # wouaw :o 
+     
+        if len(deciphered_data) < 53:                           
+            apdu = write_data_1st_step_apdu(file_no, int_to_bytes_with_padding(offset, 3), int_to_bytes_with_padding(data_len , 3), deciphered_data)
+            print "write data: ", toHexString(apdu)
+            response, sw1, sw2 = perform_command(self.__connection, apdu)
+            if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
+                raise TagException('Write data has failed!')
 
-    def __read_data(self, aid, file_no, key):
+        else:
+            apdu = write_data_1st_step_apdu(file_no, int_to_bytes_with_padding(offset, 3) , int_to_bytes_with_padding(data_len, 3), deciphered_data[0:52])
+            print "write data"
+            response, sw1, sw2 = perform_command(self.__connection, apdu)
+            if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
+                raise TagException('Write data has failed!') 
+            i = 52
+            while not i == len(deciphered_data):
+                if (i+59) > len(deciphered_data):
+                    end = len(deciphered_data)
+                else:
+                    end = i+59
+                apdu = write_data_2nd_step_apdu(deciphered_data[i:end])
+                print "write data"
+                response, sw1, sw2 = perform_command(self.__connection, apdu)
+                if not(response[len(response)-2] == 0x91 and response[len(response)-1] == 0x00 and sw1 == 0x90 and sw2 == 0x00):            
+                    raise TagException('Write data has failed!')
+            
+        
+        
+    def __read_data(self, file_no, key, encrypted):
         return None
 
     def __verify_signature(self):
@@ -180,7 +258,7 @@ class LoyaltyCard:
         #self.__km2 = DES.new(unhexlify("00"*8), DES.MODE_CBC, unhexlify("00"*8))
         #self.__kw1 = DES.new(unhexlify("00"*8), DES.MODE_CBC, unhexlify("00"*8))    
 	
-	    self.__kdesfire = unhexlify("00"*8)
+	self.__kdesfire = unhexlify("00"*8)
         self.__k = unhexlify("00"*8)
         self.__km1 = unhexlify("00"*8)
         self.__km2 = unhexlify("00"*8)
@@ -192,11 +270,16 @@ class LoyaltyCard:
         self.__select_application(0x01)
         self.__authenticate(0x00, self.__km1)       
         self.__create_file(1, 3, [0x00, 0xE1], 128)
+        sk = unhexlify(self.__authenticate(0x01, self.__kw1))
+
+        self.__write_data(1, 0, "hello world", sk)
+        
 
     def reset(self):
         self.__select_application(0)
         self.__authenticate(0, self.__kdesfire)
-        self.__delete_application(1)        
+        #self.__delete_application(1)    
+        self.__erase_memory()      
 
     def get_counter(self):
         sk = self.__authenticate(2,1,self.__k)
